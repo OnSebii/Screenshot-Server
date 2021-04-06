b7927d59c8caeb8ed4fc59d001921d6c29b58d96
OnSebii <lang.sebastian.006@gmail.com>
2021-04-06 08:50:51 +0200

    Update

--- routes/serverRoutes.js
+++ routes/serverRoutes.js
@@ -7,6 +7,7 @@ router.post(
   '/upload/',
   asyncHandler(async (req, res) => {
     const result = await uploadImage(req);
+    // TODO: req.get("host") muss ausgetauscht werden mit dem richtigen Pfad wenn man eine custom url angibt
     if (result.code == 200) res.status(result.code).send(req.get('host') + '/' + result.data);
     else {
       res.status(result.code).json(result);
